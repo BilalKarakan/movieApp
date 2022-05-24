5415d6728431ee20dc1c281be2f9e15200053bb0
Bilal Karakan <bilalkaraka36@gmail.com>
2022-05-24 11:13:55 +0300

    24.05.2022

--- movieApp.web/Startup.cs
+++ movieApp.web/Startup.cs
@@ -17,7 +17,6 @@ namespace movieApp.web
             services.AddControllersWithViews();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
